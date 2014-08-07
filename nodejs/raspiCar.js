0a34f9bf22ea4903fa2458292ad17bd1d476eb36
enverk <enver@enverk.com>
2014-08-08 00:31:17 +0300

    tum degisiklikler

--- nodejs/raspiCar.js
+++ nodejs/raspiCar.js
@@ -10,17 +10,16 @@ var io = require('socket.io').listen(8085)
 
 // Load required modules.
 var sys = require('sys'),
-	exec = require('child_process').exec
+exec = require('child_process').exec
 
 // Path to Raspbian's gpio driver, used for sending signals to the remote.
 var path = '/sys/class/gpio/',
 // Pin numbers on the Raspberry Pi connected to the car's remote.
-	pins = [23, 24, 25, 10, 9, 11];
+pins = [23, 24, 25, 10, 9, 11];
 //servo0=7=gpio4 servo1=11=gpio17 servo motorlar icin kullanılacak
 
 var servoKucuk=50;
-var servoBuyuk = 250;
-
+var servoBuyuk=250;
 
 // Enable sending signals to the car's remote control
 // which is connected to the Raspberry Pi.
@@ -38,6 +37,9 @@ function calistir(command) {
 // Listen for connections
 io.sockets.on('connection', function (socket) {
     console.log('baglanti oldu galiba');
+
+    //command = 'mjpg_streamer -i "input_uvc.so -f 10 -y YUYV" -o "output_http.so  -p 8080  -w /usr/local/www"';
+    // calistir(command);
     // Listen for direction messages from the app.
     socket.on('ping', function (data) {
         console.log('ping');
@@ -50,7 +52,7 @@ io.sockets.on('connection', function (socket) {
     socket.on('leftThrottle', function (data) {
         // console.log('leftThrottle: ' + data);
         if (data < 0.5) {
-            console.log('geri ');
+            //console.log('geri ');
             command = 'echo 1 > ' + path + 'gpio23/value';
             calistir(command);
 
@@ -58,31 +60,31 @@ io.sockets.on('connection', function (socket) {
             calistir(command);
         }
         else if (data > 0.5) {
-            console.log('ileri ');
-            command = 'echo 1 > ' + path + 'gpio23/value';
-            calistir(command);
+            //console.log('ileri ');
+                command = 'echo 1 > ' + path + 'gpio23/value';
+                calistir(command);
 
-            command = 'echo 1 > ' + path + 'gpio25/value';
-            calistir(command);
-        }
-        else {
-            console.log('dur ');
-            command = 'echo 0 > ' + path + 'gpio23/value';
-            calistir(command);
+                command = 'echo 1 > ' + path + 'gpio25/value';
+                calistir(command);
+            }
+            else {
+                //console.log('dur ');
+                command = 'echo 0 > ' + path + 'gpio23/value';
+                calistir(command);
 
-            command = 'echo 0 > ' + path + 'gpio24/value';
-            calistir(command);
+                command = 'echo 0 > ' + path + 'gpio24/value';
+                calistir(command);
 
-            command = 'echo 0 > ' + path + 'gpio25/value';
-            calistir(command);
+                command = 'echo 0 > ' + path + 'gpio25/value';
+                calistir(command);
 
-        }
+            }
     });
 
     socket.on('rightThrottle', function (data) {
         // console.log('rightThrottle: ' + data);
         if (data < 0.5) {
-            console.log('geri ');
+            //console.log('geri ');
             command = 'echo 1 > ' + path + 'gpio11/value';
             calistir(command);
 
@@ -90,7 +92,7 @@ io.sockets.on('connection', function (socket) {
             calistir(command);
         }
         else if (data > 0.5) {
-            console.log('ileri ');
+            //console.log('ileri ');
             command = 'echo 1 > ' + path + 'gpio11/value';
             calistir(command);
 
@@ -98,7 +100,7 @@ io.sockets.on('connection', function (socket) {
             calistir(command);
         }
         else {
-            console.log('dur ');
+            //console.log('dur ');
             command = 'echo 0 > ' + path + 'gpio11/value';
             calistir(command);
 
@@ -124,24 +126,34 @@ io.sockets.on('connection', function (socket) {
         calistir(command);
     });
 
-    socket.on('camStatus', function (data) {
-        // console.log('leftThrottle: ' + data);
-        if (data == 'open')
-        {
-            command = 'mjpg_streamer -i "input_uvc.so -f 10 -y YUYV" -o "output_http.so  -p 8080  -w /usr/local/www"';
-            calistir(command);
-        }
-        else if (data == 'close')
-        {
-            command = 'mjpg-streamer stop';
-            calistir(command);
-        }
-    });
 
+
+    /*
+        socket.on('camStatus', function (data) {
+            // console.log('leftThrottle: ' + data);
+            if (data == 'open')
+            {
+                command = 'mjpg_streamer -i "input_uvc.so -f 10 -y YUYV" -o "output_http.so  -p 8080  -w /usr/local/www"';
+                calistir(command);
+            }
+            else if (data == 'close')
+            {
+                command = 'mjpg-streamer stop';
+                calistir(command);
+            }
+        });
+    */
 
 })
 
 function initPins() {
+    //kamerayı calistir
+    command = 'mjpg_streamer -i "input_uvc.so -f 10 -y YUYV" -o "output_http.so  -p 8080  -w /usr/local/www"';
+    calistir(command);
+    command='sudo /usr/local/sbin/servod  --p1pins=7,11,0,0,0,0,0,0'
+    calistir(command);
+
+
     // Enable control of the Raspberry Pi's gpio pins.
     // Read more at http://elinux.org/RPi_Low-level_peripherals#Bash_shell_script.2C_using_sysfs.2C_part_of_the_raspbian_operating_system
     for (var pin in pins) {
@@ -149,7 +161,7 @@ function initPins() {
 
         // The command first checks whether the port already exists.
         var command = 'if (! [ -f ' + path + 'gpio' + pins[pin] + '/direction ]); then ' +
-			'echo ' + pins[pin] + ' > ' + path + 'export; fi';
+                        'echo ' + pins[pin] + ' > ' + path + 'export; fi';
 
         // Create the ports using Raspbian's command line.
         exec(command, function (error, stdout, stderr) {
@@ -178,5 +190,3 @@ function initPins() {
         })
     }
 }
-
-
